4f16d3d77fe71c6f756ad6bc48b5f22a46583834
Brandon Gillespie <bjg@solv.com>
2023-03-08 13:53:50 -0700

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rivet.Utils.MixProject do
   def project do
     [
       app: :rivet_utils,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.13",
       description: "Bespoke utilities for the Elixir Rivet Framework",
       source_url: "https://github.com/srevenant/rivet-utils",
