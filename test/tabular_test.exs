754fede8630d2d942ec73ffb7f8458c496815442
Jake Wood <jzwood14@yahoo.com>
2025-01-07 17:06:55 -0500

    utility for converting keywords into text-table (#8)
    * utility for converting keywords into text-table
    
    * defaults to empty string
    
    * feedback

---
+++ test/tabular_test.exs
@@ -0,0 +1,40 @@
+defmodule Rivet.Utils.TabularTest do
+  use ExUnit.Case
+  alias Rivet.Utils.Tabular
+
+  describe "tabular" do
+    test "tabularize" do
+      assert """
+             A          | B     | C
+             ---------- | ----- | -----------
+             lorem      | ipsum | dolor
+             sit        | amet  | consectetur
+             adipiscing | sed   | eiusmod
+             """ =
+               Tabular.tabularize([
+                 [a: "lorem", b: "ipsum", c: "dolor"],
+                 [a: "sit", b: "amet", c: "consectetur"],
+                 [a: "adipiscing", b: "sed", c: "eiusmod"]
+               ]) <> "\n"
+
+      assert """
+             X  | Y  | C
+             -- | -- | --
+             lo | ip | do
+             si | am | co
+             """ =
+               Tabular.tabularize([
+                 [x: "lo", y: "ip", C: "do"],
+                 [x: "si", b: "am", C: "co"]
+               ]) <> "\n"
+
+      assert """
+             I | J
+             - | -
+             a | b
+             """ = Tabular.tabularize([[i: "a", j: "b"]]) <> "\n"
+
+      assert "" == Tabular.tabularize([])
+    end
+  end
+end
