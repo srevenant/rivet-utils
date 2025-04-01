9441c8a80c35fe442506abd84e8b39b75eeb659c
Lyle Mantooth <lyle@menteeth.us>
2025-04-01 13:05:50 -0400

    String.slice/2 doesn't take negative steps on ranges.
    Restore test to make sure it works.

--- test/json_config_provider_test.exs
+++ test/json_config_provider_test.exs
@@ -10,18 +10,20 @@ defmodule Rivet.Utils.Test.JsonConfigProvider do
     File.rm!(@testfile)
   end
 
-  setup do
-    on_exit(&cleanup/0)
-  end
+  describe "JsonConfigProvider" do
+    setup do
+      on_exit(&cleanup/0)
+    end
 
-  test "config provider" do
-    assert Application.get_env(:web, :tardis) == nil
-    assert File.write!(@testfile, @testdata) == :ok
-    assert Rivet.Utils.JsonConfigProvider.init(@testfile) == @testfile
-    updates = Rivet.Utils.JsonConfigProvider.load([], @testfile)
-    assert updates == [web: [{Rivet.Utils.JsonConfigProvider, [sub: "key"]}, {:tardis, "blue"}]]
-    Application.put_all_env(updates)
-    assert Application.get_env(:web, :tardis) == "blue"
-    assert Application.get_env(:web, Rivet.Utils.JsonConfigProvider) == [{:sub, "key"}]
+    test "config provider" do
+      assert Application.get_env(:web, :tardis) == nil
+      assert File.write!(@testfile, @testdata) == :ok
+      assert Rivet.Utils.JsonConfigProvider.init(@testfile) == @testfile
+      updates = Rivet.Utils.JsonConfigProvider.load([], @testfile)
+      assert updates == [web: [{Rivet.Utils.JsonConfigProvider, [sub: "key"]}, {:tardis, "blue"}]]
+      Application.put_all_env(updates)
+      assert Application.get_env(:web, :tardis) == "blue"
+      assert Application.get_env(:web, Rivet.Utils.JsonConfigProvider) == [{:sub, "key"}]
+    end
   end
 end
