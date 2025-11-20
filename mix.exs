fe0516cf6bd84da5f17da9acd8e3841951783ea4
Lyle Mantooth <lyle@menteeth.us>
2025-11-20 11:21:44 -0500

    Secret handling library (#13)
    * Secret handling library.
    
    * Handle secrets in embedded schemas.
    
    * Recursively unseal assocs and embeds on structs.
    
    * `and_armor?/2` takes a Changeset, not the schema itself.
    
    * Skip encrypting assocs and embeds directly.

--- mix.exs
+++ mix.exs
@@ -4,8 +4,9 @@ defmodule Rivet.Utils.MixProject do
   def project do
     [
       app: :rivet_utils,
-      version: "2.0.9",
+      version: "2.1.0",
       elixir: "~> 1.14",
+      elixirc_paths: elixirc_paths(Mix.env()),
       description: "Bespoke utilities for the Elixir Rivet Framework",
       source_url: "https://github.com/srevenant/rivet-utils",
       docs: [main: "Rivet.Utils"],
@@ -30,6 +31,10 @@ defmodule Rivet.Utils.MixProject do
     [extra_applications: [:logger]]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
@@ -37,6 +42,7 @@ defmodule Rivet.Utils.MixProject do
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ecto, "~> 3.7"},
+      {:enacl, "~> 1.2", repo: "cato"},
       {:excoveralls, "~> 0.14", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:jason, "~> 1.0"},
